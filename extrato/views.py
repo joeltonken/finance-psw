666bfd53636260165902493508fd487c5c8b5513
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-07-06 16:59:46 -0300

    Export extrato pdf

--- extrato/views.py
+++ extrato/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, redirect
 from perfil.models import Categoria, Conta
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from .models import Valores
 from django.contrib import messages
 from django.contrib.messages import constants
 from datetime import datetime
+from django.template.loader import render_to_string
+import os
+from django.conf import settings
+from weasyprint import HTML
+from io import BytesIO
 
 def novo_valor(request):
     if request.method == "GET":
@@ -60,3 +65,20 @@ def view_extrato(request):
     #TODO: Botao para zerar os filtros (redirect)
     #TODO: Filtrar para o período 
     return render(request, 'view_extrato.html', {'valores': valores, 'contas': contas, 'categorias': categorias})
+
+
+def exportar_pdf(request):
+    valores = Valores.objects.filter(data__month=datetime.now().month)
+    contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
+    
+    path_template = os.path.join(settings.BASE_DIR, 'templates/partials/extrato.html')
+    path_output = BytesIO()
+
+    template_render = render_to_string(path_template, {'valores': valores, 'contas': contas, 'categorias': categorias})
+    HTML(string=template_render).write_pdf(path_output)
+
+    path_output.seek(0)
+    
+
+    return FileResponse(path_output, filename="extrato.pdf")
